2b6b068245d0c72311b58d270a3f5aede72edb96
santiago <97708495+Sargo258@users.noreply.github.com>
2023-10-03 16:03:48 -0500

    cambios a social/models, al feed y la db

--- social_django/social/views.py
+++ social_django/social/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 
 
 def feed(request):
-    return render(request, 'social/feed.html')
+    posts = Post.objects.all()
+
+    context = {'posts': posts}
+    return render(request, 'social/feed.html', context)
 
 
 def profile(request):
